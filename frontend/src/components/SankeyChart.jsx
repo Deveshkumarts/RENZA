8b2ea70ee295b6a6c09eedcd11f419f7f77b4a70
Devesh Kumar <deveshk17122006@gmail.com>
2026-07-24 11:14:27 +0530

    Remove arrows from task pipeline UI

--- frontend/src/components/SankeyChart.jsx
+++ frontend/src/components/SankeyChart.jsx
@@ -66,15 +66,6 @@ export default function SankeyChart({ tasks }) {
                 {step.label}
               </span>
             </div>
-            
-            {/* Arrow separator (don't show after the last item) */}
-            {index < flowSteps.length - 1 && (
-              <div style={{ color: 'var(--text-secondary)', opacity: 0.5, display: 'flex', alignItems: 'center' }}>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M5 12h14M12 5l7 7-7 7"/>
-                </svg>
-              </div>
-            )}
           </React.Fragment>
         ))}
       </div>
